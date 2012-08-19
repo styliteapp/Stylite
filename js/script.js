370f19fcb56e1968673177227156e4aec8f70866
Grant Standridge <grant.standridge@gmail.com>
2012-08-19 01:26:27 -0400

    completed styleboard screen

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 
 $(document).ready(function(){
 /****************************
-*							*
-*							*
-*		ACCOUNT				*
-*							*
-*							*
+*****************************
+*****************************
+********ACCOUNT**************
+*****************************
+*****************************
 ****************************/
 	var fName,
 		lName,
@@ -107,6 +107,95 @@ $(document).ready(function(){
 			.nextAll().remove()
 		;
 		$("h2 a").show();
+		return false;
+	});
+
+/****************************
+*****************************
+*****************************
+***********CREATE************
+*********STYLEBOARD**********
+*****************************
+****************************/
+	$(".boardCreation button").click(function(){
+		$("section.newItemPanel").css({
+			"left":"0",
+			"-webkit-box-shadow":"2px 5px 15px 0px rgba(0, 0, 0, .5)"
+		});
+	});
+	$(".stepThree > a").click(function(){
+		$("section.newItemPanel").css({
+			"left":"-341.5px",
+			"-webkit-box-shadow":"0px 0px 0px 0px rgba(0, 0, 0, .5)"})
+			.find(".stepTwo select")
+			.delay(150).val("")
+			.next().val("")
+			.next().val("")
+			.parent().next()
+			.html("<select name='color' id='itemArticle'>"
+				+"<option value='' selected='selected'>what color is it?</option>"
+				+"<option value='01'>red</option>"
+				+"<option value='02'>white</option>"
+				+"<option value='03'>blue</option>"
+				+"<option value='04'>yellow</option>"
+				+"<option value='05'>purple</option>"
+				+"<option value='06'>green</option>"
+				+"<option value='07'>orange</option>"
+				+"<option value='08'>brown</option>"
+				+"<option value='09'>black</option>"
+				+"<option value='10'>teal</option>"
+				+"<option value='11'>tan</option>"
+				+"<option value='12'>gray</option>"
+				+"<option value='13'>navy</option>"
+				+"</select>"
+				+"<a href=''>add another color</a>"
+			)
+		;
+		$(".newItemError").addClass("hide");
+		return false;
+	});
+	$(".stepThree button").click(function(){
+		if($("#itemArticle").val()!="" && $(".stepTwo input[name=brand]").val()!="" && $(".stepTwo input[name=store]").val()!=""){
+			$(".newItemError").addClass("hide");
+			alert("SUCCESS");
+		}else{
+			$(".newItemError").removeClass("hide");
+		}
+	});
+/*=================================
+============ADD COLORS=============
+=================================*/
+	$(".stepTwo .col2 > a").click(function(){
+		if($(this).prev().val()!=''	&& $(".stepTwo .col2 select").length < 4){
+			$(this).before("<select name='color' id='itemArticle'>"
+				+"<option value='' selected='selected'>what color is it?</option>"
+				+"<option value='01'>red</option>"
+				+"<option value='02'>white</option>"
+				+"<option value='03'>blue</option>"
+				+"<option value='04'>yellow</option>"
+				+"<option value='05'>purple</option>"
+				+"<option value='06'>green</option>"
+				+"<option value='07'>orange</option>"
+				+"<option value='08'>brown</option>"
+				+"<option value='09'>black</option>"
+				+"<option value='10'>teal</option>"
+				+"<option value='11'>tan</option>"
+				+"<option value='12'>gray</option>"
+				+"<option value='13'>navy</option>"
+				+"</select>"
+			);
+		}
+		return false;
+	});
+/*=================================
+============HELP SCREEN============
+=================================*/
+	$("header > a").click(function(){
+		$("div.helpStyleboard").removeClass("hide");
+		return false;
+	});
+	$("div.helpStyleboard").click(function(){
+		$(this).addClass("hide");
 		return false;
 	});
 });
