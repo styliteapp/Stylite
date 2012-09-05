671b99aaab968ea9cda577e86b76fa761866ab0c
Grant Standridge <grant.standridge@gmail.com>
2012-09-05 00:09:26 -0400

    styled LAUNCH section

--- fuel/app/views/index.php
+++ fuel/app/views/index.php
@@ -43,7 +43,10 @@
 			<h2>How and When You Can Learn More</h2>
 			<div class="fs3b-map"><iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Full+Sail+Parking,+3300+University+Park+Drive,+Winter+Park,+FL&amp;aq=&amp;sll=28.59735,-81.320535&amp;sspn=0.032367,0.066047&amp;ie=UTF8&amp;hq=Full+Sail+Parking,+3300+University&amp;hnear=Park+Dr,+Casselberry,+Florida+32707&amp;t=m&amp;ll=28.595487,-81.305094&amp;spn=0.003768,0.00427&amp;z=16&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Full+Sail+Parking,+3300+University+Park+Drive,+Winter+Park,+FL&amp;aq=&amp;sll=28.59735,-81.320535&amp;sspn=0.032367,0.066047&amp;ie=UTF8&amp;hq=Full+Sail+Parking,+3300+University&amp;hnear=Park+Dr,+Casselberry,+Florida+32707&amp;t=m&amp;ll=28.595487,-81.305094&amp;spn=0.003768,0.00427&amp;z=16" style="color:#0000FF;text-align:left">View Larger Map</a></small></div>
 			<p>The Stylite iPad application will go live @ <b>6p.m.</b> on <b>Thursday, Sept. 27</b>. At that time, Dont&eacute; & Grant will present the app at Full Sail University in the FS3B building auditorium. All are invited to attend the free presentation and invite your friends as well!<br /><br />If you would like to join our emailing list and stay in-the-know with regard to Stylite, shoot us your email address below and we will keep you posted about the application's status!</p>
-			<p><input type="email" id="email" name="email"></p>
+			<p>
+				<input type="email" id="email" name="email">
+				<input type="button" id="sendEmail" value="SUBMIT">
+			</p>
 		</article>
 		<article id="architects" style="display:none;">
 			<h2>The Team Behind Stylite</h2>
