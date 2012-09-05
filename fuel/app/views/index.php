8d31e378aa3f5ee32805ebd00ef5c8c4c83c105a
Grant Standridge <grant.standridge@gmail.com>
2012-09-04 23:08:42 -0400

    added google analytics

--- fuel/app/views/index.php
+++ fuel/app/views/index.php
@@ -10,6 +10,16 @@
 
 	<script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
 	<?php echo Asset::js('jquery.countdown.min.js'); ?>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-34570029-1']);
+			_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+  </script>
 </head>
 <body class="tk-apertura">
 <div id="container">
