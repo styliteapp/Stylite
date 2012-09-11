38c4fba5605467713410d767f0c774c800773b1b
Grant Standridge <grant.standridge@gmail.com>
2012-09-11 00:55:50 -0400

    blah

--- fuel/app/classes/controller/api.php
+++ fuel/app/classes/controller/api.php
@@ -84,7 +84,7 @@ class Controller_Api extends Controller_Rest
 
 	 	$this->response(array(
 	 		'success'	=>	true,
-	 		'user'		=>	array('id'=>$user->id,'email'=>$user->email,'first_name'=>$user->first_name,'last_name'=>$user->last_name)
+	 		'user'		=>	(Object) array('id'=>$user->id,'email'=>$user->email,'first_name'=>$user->first_name,'last_name'=>$user->last_name)
 	 	));
  	}
 }
