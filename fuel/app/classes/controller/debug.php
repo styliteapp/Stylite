7d1910210d62a2ee4e565baa23e74aea6e793768
Grant Standridge <grant.standridge@gmail.com>
2012-09-13 17:58:35 -0400

    added debug view

---
+++ fuel/app/classes/controller/debug.php
@@ -0,0 +1,19 @@
+<?php
+
+class Controller_Debug extends Controller
+{
+	public function action_logs($year = null, $month = null, $day = null)
+	{
+		$date = date("Y/m/d");
+
+		if (Fuel::$env == Fuel::PRODUCTION) $path = '/var/www/styliteapp.com';
+		if (Fuel::$env == Fuel::DEVELOPMENT) $path = '/Users/grantes/Sites/Stylite_app';
+
+		$input = file($path . '/fuel/app/logs/' . $date . '.php');
+
+		return Response::forge(View::forge('debug/logs', array(
+			'logs' => array_reverse($input),
+		)));
+		
+	}
+}
---
+++ fuel/app/views/debug/logs.php
@@ -0,0 +1,13 @@
+<style type="text/css">
+p {
+	font-family: "Lucida Grande";
+	line-height: 1.4;
+	font-size: 14px;
+	border-bottom: 1px solid #ccc; padding: 0 0 8px; margin: 0 0 8px;
+}
+</style>
+
+<?php foreach ($logs as $log): ?>
+	<p><?php echo $log ?></p>
+<?php endforeach; ?>
+</p>
