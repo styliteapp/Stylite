8aa4489b29ac9ebc7232a74cf217b2c510fdb200
Grant Standridge <grant.standridge@gmail.com>
2012-09-13 00:14:04 -0400

    x

--- fuel/app/classes/controller/api.php
+++ fuel/app/classes/controller/api.php
@@ -111,14 +111,7 @@ class Controller_Api extends Controller_Rest
 
 	 	$sizes = Image::sizes(DOCROOT.'uploads/l/'.$imgName.'.jpg');
 
-	 	if($sizes->width > $sizes->height)
-	 	{
-			Image::load(DOCROOT.'uploads/l/'.$imgName.'.jpg')
-	 			->resize(200);
-	 	}else{
-		 	Image::load(DOCROOT.'uploads/l/'.$imgName.'.jpg')
-	 			->resize(null, 200);
-	 	}
+	 	Image::load(DOCROOT.'uploads/l/'.$imgName.'.jpg', true)->resize(200);
 
 	 	if( !$success ){
 		 	$this->response(array(
