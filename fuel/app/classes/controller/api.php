d8ab070d6d13f555d4495310dbcac0e88f0811d1
Grant Standridge <grant.standridge@gmail.com>
2012-09-14 20:03:39 -0400

    edited img

--- fuel/app/classes/controller/api.php
+++ fuel/app/classes/controller/api.php
@@ -117,17 +117,17 @@ class Controller_Api extends Controller_Rest
 		}else{
 			Image::load(DOCROOT.'uploads/l/'.$imgName.'.jpg')->resize('7.7160494%', '7.7160494%')->save(DOCROOT.'uploads/s/'.$imgName.'.jpg');
 			$imgSizes = Image::sizes(DOCROOT.'uploads/s/'.$imgName.'.jpg');
-			//$imgSize = $imgSizes->width > $imgSizes->height ? 'landscape' : 'portrait';
-			//$dbSave = Model_Upload::add(Input::post('user_id'), $imgName.'.jpg', $imgSize);
+			$imgSize = ($imgSizes->width > $imgSizes->height) ? 'landscape' : 'portrait';
+			$dbSave = Model_Upload::add(Input::post('user_id'), $imgName.'.jpg', $imgSize);
 		}
 	
-		/*if( ! $dbSave )
+		if( ! $dbSave )
 		{
 			$this->response(array(
 				'success'	=> false,
 				'message'	=> 'bad upload'
 			));
-		}*/
+		}
 	
 		$this->response(array(
 			'success'	=> true,
